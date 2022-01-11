c93cc6d85a21ac239ce99a1c2046e10cb5573869
Michal99Zajac <michal.99.zajac@gmail.com>
2022-01-12 00:14:27 +0100

    feat(Error): add error page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ThemeProvider from '@workcode/components'
 import { Routes, Route, Navigate } from 'react-router-dom'
 
-import { NotFound, Index } from './other'
+import { NotFound, Index, Error } from './other'
 import { UserConfig } from './config'
 import { Menu, Wait, Editor } from './workspace'
 import { ChangePassword, ForgotPassword, SignIn, SignUp } from './auth'
@@ -29,6 +29,7 @@ export function App(): JSX.Element {
               <Route index element={<Navigate to="/auth/signin" />} />
             </Route>
             <Route path="config" element={<UserConfig />} />
+            <Route path="error" element={<Error />} />
             <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
