3ce55717a3e2e7698a6c94049634be86168e23fd
Michal99Zajac <michal.99.zajac@gmail.com>
2022-01-12 00:42:18 +0100

    feat(ErrorBaundary): add ErrorBaundary to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ThemeProvider from '@workcode/components'
 import { Routes, Route, Navigate } from 'react-router-dom'
+import { ErrorBoundary } from 'react-error-boundary'
 
 import { NotFound, Index, Error } from './other'
 import { UserConfig } from './config'
@@ -11,29 +12,30 @@ import Guardian from './Guardian'
 export function App(): JSX.Element {
   return (
     <ThemeProvider>
-      <Guardian>
-        <Routes>
-          <Route path="/">
-            <Route index element={<Index />} />
-            <Route path="workspace">
-              <Route path="menu" element={<Menu />} />
-              <Route path="wait" element={<Wait />} />
-              <Route path="editor" element={<Editor />} />
-              <Route index element={<Navigate to="/workspace/menu" />} />
+      <ErrorBoundary FallbackComponent={Error}>
+        <Guardian>
+          <Routes>
+            <Route path="/">
+              <Route index element={<Index />} />
+              <Route path="workspace">
+                <Route path="menu" element={<Menu />} />
+                <Route path="wait" element={<Wait />} />
+                <Route path="editor" element={<Editor />} />
+                <Route index element={<Navigate to="/workspace/menu" />} />
+              </Route>
+              <Route path="auth">
+                <Route path="change-password" element={<ChangePassword />} />
+                <Route path="forgot-password" element={<ForgotPassword />} />
+                <Route path="signin" element={<SignIn />} />
+                <Route path="signup" element={<SignUp />} />
+                <Route index element={<Navigate to="/auth/signin" />} />
+              </Route>
+              <Route path="config" element={<UserConfig />} />
+              <Route path="*" element={<NotFound />} />
             </Route>
-            <Route path="auth">
-              <Route path="change-password" element={<ChangePassword />} />
-              <Route path="forgot-password" element={<ForgotPassword />} />
-              <Route path="signin" element={<SignIn />} />
-              <Route path="signup" element={<SignUp />} />
-              <Route index element={<Navigate to="/auth/signin" />} />
-            </Route>
-            <Route path="config" element={<UserConfig />} />
-            <Route path="error" element={<Error />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
-      </Guardian>
+          </Routes>
+        </Guardian>
+      </ErrorBoundary>
     </ThemeProvider>
   )
 }
--- src/other/pages/Error/Error.tsx
+++ src/other/pages/Error/Error.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { FallbackProps } from 'react-error-boundary'
 
-export function Error(): JSX.Element {
-  return <div>Error Page</div>
+export function Error(props: FallbackProps): JSX.Element {
+  const { error, resetErrorBoundary } = props
+  return (
+    <div>
+      <div>{error.message}</div>
+      <button onClick={resetErrorBoundary}>reset</button>
+    </div>
+  )
 }
 
 export default Error
