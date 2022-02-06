c3ccf6ad2ebb1073ada6071ae37ba0724f334007
Michal99Zajac <michal.99.zajac@gmail.com>
2022-02-06 14:52:36 +0100

    feat(invite): upgrade invite statuses

--- src/workspace/components/InviteCard/InviteCard.tsx
+++ src/workspace/components/InviteCard/InviteCard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import {
   Avatar,
   useColorMode,
@@ -15,6 +15,15 @@ import { UserType } from '../../../common/schemas'
 
 import classes from './InviteCard.module.scss'
 
+/**
+ * isInvited statuses:
+ *
+ * -1 = invited
+ * 0 = never invited invaited
+ * 1 = invited once or more
+ */
+type IsInvited = -1 | 0 | 1
+
 interface InviteCardProps {
   user: UserType
 }
@@ -22,15 +31,29 @@ interface InviteCardProps {
 export function InviteCard(props: InviteCardProps): JSX.Element {
   const { user } = props
   const { colorMode } = useColorMode()
-  const [isInvited, setIsInvited] = useState(false)
+  const [isInvited, setIsInvited] = useState<IsInvited>(0)
   const isDark = colorMode === 'dark'
+  const isNotInvited = isInvited !== -1
+
+  const getInviteStatus = useCallback(() => {
+    switch (isInvited) {
+      case 0:
+        return 'invite'
+      case 1:
+        return 'invite again'
+      case -1:
+        return 'invited'
+      default:
+        throw new Error('isInvited is out of range')
+    }
+  }, [isInvited])
 
   useEffect(() => {
     let timeout: NodeJS.Timeout
 
     if (isInvited) {
       timeout = setTimeout(() => {
-        setIsInvited(false)
+        setIsInvited(1)
       }, 6000)
     }
 
@@ -44,18 +67,18 @@ export function InviteCard(props: InviteCardProps): JSX.Element {
       className={clsx(
         classes.card,
         isDark ? classes.darkCard : classes.lightCard,
-        isInvited && classes.cardInvited
+        !isNotInvited && classes.cardInvited
       )}
     >
       <Avatar
         size="sm"
-        src={!isInvited ? user.src || undefined : undefined}
-        name={!isInvited ? `${user.firstname} ${user.lastname}` : undefined}
-        icon={isInvited ? <CheckIcon /> : undefined}
+        src={isNotInvited ? user.src || undefined : undefined}
+        name={isNotInvited ? `${user.firstname} ${user.lastname}` : undefined}
+        icon={!isNotInvited ? <CheckIcon /> : undefined}
         className={clsx(
           classes.invitedAvatar,
           user.src && classes.isSrcAvatar,
-          isInvited &&
+          !isNotInvited &&
             (isDark ? classes.invitedAvatarDark : classes.invitedAvatarLight)
         )}
       />
@@ -69,12 +92,12 @@ export function InviteCard(props: InviteCardProps): JSX.Element {
       </Stack>
       <Spacer />
       <Button
-        width="100px"
-        isDisabled={isInvited}
-        onClick={() => setIsInvited(true)}
-        className={clsx(isInvited && classes.invitedButton)}
+        width="min-content"
+        isDisabled={isInvited === -1}
+        onClick={() => setIsInvited(-1)}
+        className={clsx(!isNotInvited && classes.invitedButton)}
       >
-        {isInvited ? 'invited' : 'invite'}
+        {getInviteStatus()}
       </Button>
     </Flex>
   )
