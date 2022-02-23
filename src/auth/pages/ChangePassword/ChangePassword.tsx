889d82e47bb780e31a246b1feedb353143cea69a
Michal99Zajac <michal.99.zajac@gmail.com>
2022-02-23 20:36:40 +0100

    refactor(auth): implement new style to the change password page

--- src/auth/pages/ChangePassword/ChangePassword.tsx
+++ src/auth/pages/ChangePassword/ChangePassword.tsx
@@ -1,10 +1,17 @@
 import React, { useState } from 'react'
-import { Box, Button, Input, InputGroup, Text } from '@chakra-ui/react'
+import {
+  Button,
+  Center,
+  Input,
+  InputGroup,
+  Stack,
+  Text,
+} from '@chakra-ui/react'
 import { Controller, useForm } from 'react-hook-form'
 import { useNavigate } from 'react-router-dom'
 import { zodResolver } from '@hookform/resolvers/zod'
 
-import { DragPocket, Window } from '../../../common/components'
+import { Window } from '../../../common/components'
 import { useToast } from '../../../common/hooks'
 import { changePassword, Form, Fail, FormType } from '../../api/changePassword'
 
@@ -38,65 +45,55 @@ export function ChangePassword(): JSX.Element {
   })
 
   return (
-    <Box>
-      <DragPocket>
-        <Box position="absolute">
-          <Window title="Change Password" onClick={() => navigate('/')}>
-            <form onSubmit={onSubmit}>
-              <Controller
-                control={control}
-                name="password"
-                render={({ field, fieldState }) => (
-                  <InputGroup
-                    display="flex"
-                    flexDirection="column"
-                    marginBottom={5}
-                  >
-                    <Text fontSize="sm">* New Password</Text>
-                    <Input
-                      type="password"
-                      isDisabled={isLoading}
-                      placeholder="8S!cHGp3"
-                      onChange={field.onChange}
-                      isInvalid={fieldState.invalid}
-                      ref={field.ref}
-                    />
-                  </InputGroup>
-                )}
-              />
-              <Controller
-                control={control}
-                name="repeatedPassword"
-                render={({ field, fieldState }) => (
-                  <InputGroup
-                    display="flex"
-                    flexDirection="column"
-                    marginBottom={5}
-                  >
-                    <Text fontSize="sm">* Repeat Password</Text>
-                    <Input
-                      type="password"
-                      isDisabled={isLoading}
-                      placeholder="8S!cHGp3"
-                      onChange={field.onChange}
-                      isInvalid={fieldState.invalid}
-                      ref={field.ref}
-                    />
-                  </InputGroup>
-                )}
-              />
-              <Button
-                isLoading={isLoading}
-                type="submit"
-                onClick={() => runToast(formState.errors, 'Error', 'error')}
-              >
-                change password
-              </Button>
-            </form>
-          </Window>
-        </Box>
-      </DragPocket>
-    </Box>
+    <Center w="100%" h="100%">
+      <Window title="Change Password" onClick={() => navigate('/')}>
+        <form onSubmit={onSubmit}>
+          <Stack mt={4} minW="340px" w="340px" spacing={5}>
+            <Controller
+              control={control}
+              name="password"
+              render={({ field, fieldState }) => (
+                <InputGroup display="flex" flexDirection="column">
+                  <Text fontSize="sm">* New Password</Text>
+                  <Input
+                    type="password"
+                    isDisabled={isLoading}
+                    placeholder="8S!cHGp3"
+                    onChange={field.onChange}
+                    isInvalid={fieldState.invalid}
+                    ref={field.ref}
+                  />
+                </InputGroup>
+              )}
+            />
+            <Controller
+              control={control}
+              name="repeatedPassword"
+              render={({ field, fieldState }) => (
+                <InputGroup display="flex" flexDirection="column">
+                  <Text fontSize="sm">* Repeat Password</Text>
+                  <Input
+                    type="password"
+                    isDisabled={isLoading}
+                    placeholder="8S!cHGp3"
+                    onChange={field.onChange}
+                    isInvalid={fieldState.invalid}
+                    ref={field.ref}
+                  />
+                </InputGroup>
+              )}
+            />
+            <Button
+              isLoading={isLoading}
+              type="submit"
+              onClick={() => runToast(formState.errors, 'Error', 'error')}
+            >
+              change password
+            </Button>
+          </Stack>
+        </form>
+      </Window>
+    </Center>
   )
 }
 
