f163248d6c1173a489efe7adc4b16ddd54f57a6d
Michal99Zajac <michal.99.zajac@gmail.com>
2022-02-25 22:48:47 +0100

    refactor(workspace): remove ModalWindow from contributors

--- src/workspace/components/MenuCardContributors/MenuCardContributors.tsx
+++ src/workspace/components/MenuCardContributors/MenuCardContributors.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import { SettingsIcon } from '@chakra-ui/icons'
-import { MenuItem, Stack, useDisclosure } from '@chakra-ui/react'
+import {
+  MenuItem,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Stack,
+  useDisclosure,
+} from '@chakra-ui/react'
 
 import { ContributorSetter } from '../ContributorSetter'
-import { ModalWindow } from '../../../common/components'
 import { WorkspaceType } from '../../schemas/Workspace'
 
 interface MenuCardContributorsProps {
@@ -21,17 +30,24 @@ export function MenuCardContributors(
       <MenuItem onClick={onOpen}>
         <SettingsIcon mr={4} /> Contributors
       </MenuItem>
-      <ModalWindow title="Contributors" onClose={onClose} isOpen={isOpen}>
-        <Stack maxH="75vh" overflow="auto" px={2}>
-          {workspace.contributors.map((contributor) => (
-            <ContributorSetter
-              key={contributor.id}
-              contributor={contributor}
-              setContributor={(c) => alert(JSON.stringify(c))}
-            />
-          ))}
-        </Stack>
-      </ModalWindow>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Contributors</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Stack>
+              {workspace.contributors.map((contributor) => (
+                <ContributorSetter
+                  key={contributor.id}
+                  contributor={contributor}
+                  setContributor={(c) => alert(JSON.stringify(c))}
+                />
+              ))}
+            </Stack>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </>
   )
 }
