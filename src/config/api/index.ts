7fab89b8f0be8b055c010f45697b3ff9763ea08f
Michal99Zajac <michal.99.zajac@gmail.com>
2022-05-04 20:22:15 +0200

    refactor(config): remove updateData mock function

--- src/config/api/index.ts
+++ src/config/api/index.ts
@@ -1,5 +1,4 @@
 export { deleteAccount } from './deleteAccount'
-export { updateUserData } from './updateUserData'
 export { useMe } from './useMe'
 export { useUpdatePassword } from './useUpdatePassword'
 export * as useUpdateMe from './useUpdateMe'
