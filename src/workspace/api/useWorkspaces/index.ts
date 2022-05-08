88c7fe1a99a8b090c12eb27bb1a0b375abf59096
Michal99Zajac <michal.99.zajac@gmail.com>
2022-05-08 10:44:10 +0200

    feat(workspace): add makeQuery util

--- src/workspace/api/useWorkspaces/index.ts
+++ src/workspace/api/useWorkspaces/index.ts
@@ -2,21 +2,17 @@ import { useQuery } from 'react-query'
 
 import { api } from 'api'
 import { createCodec } from 'codec'
+import { makeQuery } from 'workspace/utils'
 
 import { Response, ErrorResponse, Form } from './schema'
 
 export const useWorkspaces = (form: Form) => {
   const codec = createCodec(Response, ErrorResponse)
-  const params: Record<string, string> = {}
-  if (form.self) params.self = 'true'
-  if (form.name) params.name = form.name
-  // if (form.owner) params.owner = form.owner // TODO: after backend fix uncomment
-  if (form.code && form.code !== 'ALL') params.code = form.code
-  const query = new URLSearchParams(params)
+  const query = makeQuery(form)
 
   return useQuery<Response, ErrorResponse>({
     queryKey: ['workspace_useWorkspaces__get__', form],
-    queryFn: () => codec(api.get(`workspaces?${query.toString()}`)),
+    queryFn: () => codec(api.get(`workspaces${query}`)),
     initialData: [],
   })
 }
