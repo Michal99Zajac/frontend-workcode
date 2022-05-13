5a39e92068b90bfbffab80a12df74dfe3dcbcab2
Michal99Zajac <michal.99.zajac@gmail.com>
2022-05-13 18:02:20 +0200

    refactor(routers): remove workspace and editor provider

--- src/editor/api/index.ts
+++ src/editor/api/index.ts
@@ -1,2 +1,4 @@
 export { getContributors } from './getContributors'
 export { sendMessage } from './sendMessage'
+
+export * as editor from './useEditor'
--- src/editor/pages/Editor/Editor.tsx
+++ src/editor/pages/Editor/Editor.tsx
@@ -2,14 +2,22 @@ import React from 'react'
 import { Box, Flex } from '@chakra-ui/react'
 import { Navigate } from 'react-router-dom'
 
+import { useEditor } from 'editor/api/useEditor'
 import { CodeEditor, EditorBar } from 'editor/components'
 import { EditorSockProvider } from 'editor/connection'
 import { useParams } from 'react-router-dom'
 
 export function Editor(): JSX.Element {
   const { workspaceId } = useParams()
+  const { data, isLoading, isError } = useEditor({
+    workspaceId: workspaceId ?? '',
+  })
 
   if (!workspaceId) return <Navigate to="/workspace" />
+
+  if (isError) return <Navigate to="/workspace" />
+
+  if (isLoading) return <div>Loading...</div>
 
   return (
     <EditorSockProvider workspaceId={workspaceId}>
