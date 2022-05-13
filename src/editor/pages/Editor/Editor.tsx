66c511f6ccee02aff4796919496efb01d58a6224
Michal99Zajac <michal.99.zajac@gmail.com>
2022-05-13 19:06:18 +0200

    feat(editor): implement editor provider

--- src/editor/pages/Editor/Editor.tsx
+++ src/editor/pages/Editor/Editor.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Box, Flex } from '@chakra-ui/react'
 import { Navigate } from 'react-router-dom'
 
-import { WorkspaceProvider } from 'editor/context'
+import { WorkspaceProvider, EditorProvider } from 'editor/context'
 import { CodeEditor, EditorBar } from 'editor/components'
 import { EditorSockProvider } from 'editor/connection'
 import { useParams } from 'react-router-dom'
@@ -15,12 +15,14 @@ export function Editor(): JSX.Element {
   return (
     <WorkspaceProvider workspaceId={workspaceId}>
       <EditorSockProvider workspaceId={workspaceId}>
-        <Flex h="100vh" w="100%" maxH="100%" flexDirection="column">
-          <Box flexGrow={1} overflow="auto">
-            <CodeEditor />
-          </Box>
-          <EditorBar />
-        </Flex>
+        <EditorProvider>
+          <Flex h="100vh" w="100%" maxH="100%" flexDirection="column">
+            <Box flexGrow={1} overflow="auto">
+              <CodeEditor />
+            </Box>
+            <EditorBar />
+          </Flex>
+        </EditorProvider>
       </EditorSockProvider>
     </WorkspaceProvider>
   )
