97aefebdce071f03d36f5f975b0669028f77b64b
Michal99Zajac <michal.99.zajac@gmail.com>
2022-05-22 19:33:39 +0200

    refactor(index): remove unused parts of code

--- src/common/components/DragPocket/index.ts
+++
@@ -1,2 +0,0 @@
-export { default } from './DragPocket'
-export * from './DragPocket'
--- src/workspace/schemas/WorkspaceRole/index.ts
+++
@@ -1,5 +0,0 @@
-import { z } from 'zod'
-
-export const WorkspaceRole = z.enum(['WATCHER', 'EDITOR', 'ADMIN'])
-
-export type WorkspaceRole = z.infer<typeof WorkspaceRole>
